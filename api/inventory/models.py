22e46c04494d8cb2e9b09ff72107656b9815da79
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-04 22:26:06 +0000

    code 6-3-6

--- api/inventory/models.py
+++ api/inventory/models.py
@@ -23,3 +23,15 @@ class Purchase(models.Model):
     class Meta:
         db_table = 'purchase'
         verbose_name = '仕入'
+
+class Sales(models.Model):
+    """
+    売上
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(verbose_name='数量')
+    sales_date = models.DateTimeField(verbose_name='売上日時')
+
+    class Meta:
+        db_table = 'sales'
+        verbose_name = '売上'
