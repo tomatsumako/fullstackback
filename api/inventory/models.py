273502355efe22ff36aa43f4967f5d8e41383032
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-04 15:52:15 +0000

    code 6-3-4

--- api/inventory/models.py
+++ api/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    商品
+    """
+    name = models.CharField(max_length=100, verbose_name='商品名')
+    price = models.IntegerField(verbose_name='価格')
+    description = models.TextField(verbose_name='商品説明', null=True, blank=True)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = '商品'
