c305c3e536a6339845ab3fc1498498e2eedac0af
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-11 17:42:41 +0000

    code 6-5-2, 6-5-3, 6-5-4

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
 
-# Create your views here.
+class ProductView(APIView):
+    """
+    商品操作に関する関数
+    """
+    def get(self, request, format=None):
+        """
+        商品の一覧を取得する
+        """
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
