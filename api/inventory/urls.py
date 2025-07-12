ccb7c01a10f0918b64c658ca52c047d671c65a40
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-12 08:16:55 +0000

    code 6-7-1, 6-7-2, 6-7-3

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('products/', views.ProductView.as_view()),
     path('products/<int:id>/', views.ProductView.as_view()),
     path('products/model/', views.ProductModelViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('inventories/<int:id>/', views.InventoryView.as_view()),
     path('purchases/', views.PurchaseView.as_view()),
     path('sales/', views.SalesView.as_view()),
 ]
--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,8 +1,9 @@
+from django.db.models import F, Value
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product, Purchase, Sales
-from .serializers import ProductSerializer, PurchaseSerializer, SaleSerializer
+from .serializers import InventorySerializer, ProductSerializer, PurchaseSerializer, SaleSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
@@ -85,3 +86,17 @@ class SalesView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+class InventoryView(APIView):
+    # 仕入れ・売上情報を取得する
+    def get(self, request, id=None, format=None):
+        if id is None :
+            # 件数が多くなるので商品IDは必ず指定する
+            return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
+        else:
+            # UNIONするために、それぞれフィールド名を再定義している
+            purchase = Purchase.objects.filter(product_id=id).prefetch_related('product').values("id", "quantity", type=Value('1'), date=F('purchase_date'),unit=F('product__price'))
+            sales = Sales.objects.filter(product_id=id).prefetch_related('product').values("id", "quantity", type=Value('2'), date=F('sales_date'),unit=F('product__price'))
+            queryset = purchase.union(sales).order_by(F("date"))
+            serializer = InventorySerializer(queryset, many=True)
+            return Response(serializer.data, status.HTTP_200_OK)
