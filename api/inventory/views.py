95b43c24dcfee42d2eada46117bf972fc56d4357
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-12 02:03:38 +0000

    code 6-6-1

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -17,6 +17,17 @@ class ProductView(APIView):
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
 
+    def post(self, request, format=None):
+        """
+        商品を登録する
+        """
+        serializer = ProductSerializer(data=request.data)
+        # validationを通らなかった場合、例外を投げる
+        serializer.is_valid(raise_exception=True)
+        # 検証したデータを永続化する
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
 class ProductModelViewSet(ModelViewSet):
     """
     商品操作に関する関数（ModelViewSet）
