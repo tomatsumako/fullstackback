f44c209d3de3b147ebe6835d2790810731412f6c
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-12 02:37:49 +0000

    code 6-6-9

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductView.as_view()),
-    path('products/model/', views.ProductModelViewSet.as_view({'get': 'list'})),
+    path('products/model/', views.ProductModelViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('purchases/', views.PurchaseView.as_view()),
     path('sales/', views.SalesView.as_view()),
 ]
