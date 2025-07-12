3e3b035e0a2da84911370f26a083eb3f3faef7c4
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-12 02:43:53 +0000

    code 6-6-10, 6-6-11

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductView.as_view()),
+    path('products/<int:id>/', views.ProductView.as_view()),
     path('products/model/', views.ProductModelViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('purchases/', views.PurchaseView.as_view()),
     path('sales/', views.SalesView.as_view()),
--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product, Purchase, Sales
@@ -6,15 +7,27 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
 class ProductView(APIView):
+
+    # 商品操作に関する関数で共通で使用する商品取得関数
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise NotFound
+
     """
     商品操作に関する関数
     """
-    def get(self, request, format=None):
+    def get(self, request, id=None, format=None):
         """
         商品の一覧を取得する
         """
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
+        if id is None :
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+        else:
+            product = self.get_object(id)
+            serializer = ProductSerializer(product)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, request, format=None):
@@ -27,6 +40,16 @@ class ProductView(APIView):
         # 検証したデータを永続化する
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def put(self, request, id, format=None):
+        """
+        商品を更新する
+        """
+        product = self.get_object(id)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
 
 class ProductModelViewSet(ModelViewSet):
     """
