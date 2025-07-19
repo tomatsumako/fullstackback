011e0a53b8606259a54140e30a952e0fa235ec79
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-07-19 08:08:23 +0000

    code 6-8-1, 6-8-2

--- api/inventory/urls.py
+++ api/inventory/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('products/', views.ProductView.as_view()),
     path('products/<int:id>/', views.ProductView.as_view()),
     path('products/model/', views.ProductModelViewSet.as_view({'get': 'list', 'post': 'create'})),
--- config/settings/base.py
+++ config/settings/base.py
@@ -139,3 +139,10 @@ LOGGING = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+}
